f6fb351854f46689992ee6b8529705ef346bb866
huseyintekin <huseyintekin10@gmail.com>
2023-05-19 11:49:22 +0200

    life-cycle-methods

---
+++ class-notes/DE-08-lifeCycle/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import LifeCycleMethods from './components/LifeCycleMethods';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className='container text-center mt-4'>
+        <LifeCycleMethods/>
+    </div>
+  );
+}
+
+export default App;
