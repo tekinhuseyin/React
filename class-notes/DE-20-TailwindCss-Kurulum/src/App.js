6221e5018773a8c501ded22cdd9adc55c53414be
huseyintekin <huseyintekin10@gmail.com>
2023-06-07 14:08:02 +0200

    tailWindCss

---
+++ class-notes/DE-20-TailwindCss-Kurulum/src/App.js
@@ -0,0 +1,63 @@
+function App() {
+  return (
+    <div className="main">
+      {/* <h1 className="text-3xl font-bold underline">Hello</h1> */}
+      {/* <div className="flex flex-col justify-between items-center bg-slate-500 rounded-xl w-3/12 p-4">
+        <h2>Title</h2>
+        <img
+          src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1LRLLWGvs5sZdTzuMqLEahb88Pc.jpg"
+          className="h-[250px] rounded-xl"
+          alt=""
+        />
+        <p className="text-white line-clamp-1 hover:line-clamp-none">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus,
+          fugiat? Eos, temporibus enim animi dolorem laudantium aliquid delectus
+          accusantium ratione, odio maiores excepturi molestiae laboriosam?
+        </p>
+      </div> */}
+      {/* <div className="flex flex-col justify-between items-center bg-slate-500 rounded-xl w-3/12 p-4">
+        <h2>Title</h2>
+        <img
+          src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1LRLLWGvs5sZdTzuMqLEahb88Pc.jpg"
+          className="h-[250px] rounded-xl"
+          alt=""
+        />
+        <p className="text-white line-clamp-1 hover:line-clamp-none">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus,
+          fugiat? Eos, temporibus enim animi dolorem laudantium aliquid delectus
+          accusantium ratione, odio maiores excepturi molestiae laboriosam?
+        </p>
+      </div> */}
+      <div className="card">
+        <h2>Title</h2>
+        <img
+          src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1LRLLWGvs5sZdTzuMqLEahb88Pc.jpg"
+          className="h-[250px] rounded-xl"
+          alt=""
+        />
+        <p className="text-white line-clamp-1 hover:line-clamp-none">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus,
+          fugiat? Eos, temporibus enim animi dolorem laudantium aliquid delectus
+          accusantium ratione, odio maiores excepturi molestiae laboriosam?
+        </p>
+      </div>
+      <div className="card">
+        <h2>Title</h2>
+        <img
+          src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1LRLLWGvs5sZdTzuMqLEahb88Pc.jpg"
+          className="h-[250px] rounded-xl"
+          alt=""
+        />
+        <p className="text-white line-clamp-1 hover:line-clamp-none">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus,
+          fugiat? Eos, temporibus enim animi dolorem laudantium aliquid delectus
+          accusantium ratione, odio maiores excepturi molestiae laboriosam?
+        </p>
+      </div>
+      <h1>Deneme</h1>
+      <h2 className="text-4xl">Deneme</h2>
+    </div>
+  );
+}
+
+export default App;
